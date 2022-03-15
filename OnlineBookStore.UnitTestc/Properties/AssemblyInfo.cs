229f7ebbf44c3742bd92ca7a97c2e7fe34fdfb28
Helc1ng <100525141+Helc1ng@users.noreply.github.com>
2022-03-16 01:34:49 +0300

    Добавьте файлы проекта.

---
+++ OnlineBookStore.UnitTestc/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OnlineBookStore.UnitTestc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OnlineBookStore.UnitTestc")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da30a9f8-e0e8-4320-9e37-256c621230c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
